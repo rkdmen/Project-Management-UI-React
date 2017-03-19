c07b61916bdf7f0e6b5619fadb517aa237c5b764
John KM Jung <min85da@gmail.com>
2017-03-19 01:10:55 -0400

    chore: n: new project container

--- src/containers/NewProjectContainer/NewProjectModalContainer.js
+++ src/containers/NewProjectContainer/NewProjectModalContainer.js
@@ -2,7 +2,7 @@ import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { getDetail } from '../../actions/projectActions';
-import { Button, Modal, OverlayTrigger } from 'react-bootstrap';
+import { Button, Modal } from 'react-bootstrap';
 import { browserHistory } from 'react-router';
 
 
@@ -35,19 +35,13 @@ export class NewProjectModalContainer extends Component {
             <Modal.Title>Modal heading</Modal.Title>
           </Modal.Header>
           <Modal.Body>
-            <h4>Text in a modal</h4>
-            <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula.</p>
-
-            <h4>Popover in a modal</h4>
-
+            <h4>New Project</h4>
             <hr />
 
-            <h4>Overflowing text to show scroll behavior</h4>
             <p>Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.</p>
-            <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.</p>
-            <p>Aenean lacinia bibendum nulla sed consectetur. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Donec sed odio dui. Donec ullamcorper nulla non metus auctor fringilla.</p>
-            <p>Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.</p>
-            <p>Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.</p>
+            <p>Praesent  sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.</p>
+
+
           </Modal.Body>
           <Modal.Footer>
             <Button onClick={this.close}>Close</Button>
